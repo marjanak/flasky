68b8528a5e49e4ed1d4e07593f52038c4648187f
marjanak <marjana1816@gmail.com>
2024-11-05 15:49:56 -0800

    refactoring

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint,abort,make_response,request,Response
 # from ..models.cat import cats
 from ..db import db
 from app.models.cat import Cat
+from .route_utilities import validate_model
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix="/cats")
 
@@ -9,18 +10,19 @@ cats_bp = Blueprint("cats_bp", __name__, url_prefix="/cats")
 @cats_bp.post("")
 def create_cat():
     request_body = request.get_json()
-    name = request_body["name"]
-    color = request_body ["color"]
-    personality = request_body ["personality"]
-
-
-    new_cat = Cat(name=name, color=color, personality=personality)
-
+    try:
+        new_cat = Cat.from_dict(request_body)
+        
+        response = new_cat.to_dict()
+        return response, 201
+    except KeyError as e:
+        reponse = {"message": f"Invalid request:missing {e.args[0]}"}
+        abort(make_response(response, 400))
     db.session.add(new_cat)
     db.session.commit()
 
-    response = new_cat.to_dict()
-    return response, 201
+    reponse = new_cat.to_dict()
+    return respose,201
 
 @cats_bp.get("")
 def get_all_cats():
@@ -44,13 +46,13 @@ def get_all_cats():
 
 @cats_bp.get("/<cat_id>")
 def get_single_cat(cat_id):
-    cat = validate_cat(cat_id)
+    cat = validate_model(cat_id)
 
     return cat.to_dict()
 
 @cats_bp.put("/<cat_id>")
 def update_cat(cat_id):
-    cat = validate_cat(cat_id)
+    cat = validate_model(cat_id)
     request_body = request.get_json()
 
     cat.name = request_body["name"]
@@ -63,23 +65,10 @@ def update_cat(cat_id):
 
 @cats_bp.delete("/<cat_id>")
 def delete_cat(cat_id):
-    cat = validate_cat(cat_id)
+    cat = validate_model(cat_id)
 
     db.session.delete(cat)
     db.session.commit()
 
     return Response(status=204, mimetype="application/json")
 
-def validate_cat(cat_id):
-    try:
-        cat_id = int(cat_id)
-    except:
-        abort(make_response({"message": f"Cat id {cat_id} inva;id,400"}))
-
-    query = db.select(Cat).where(Cat.id == cat_id)
-    cat = db.session.scalar(query)
-
-    if not cat:
-        abort(make_response({"message":f"Cat {cat_id} not found,404"}))
-    
-    return cat
